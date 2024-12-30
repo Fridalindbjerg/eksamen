6af44834be9d149c7ff679ff309a3a5389c18c5d
Frida Lindbjerg <fridalindbjerg@outlook.dk>
2024-12-30 18:31:48 +0530

    media index

--- js/js.js
+++ js/js.js
@@ -1,15 +1,15 @@
-document.querySelectorAll(".header_nav li").forEach((li) => {
-  li.addEventListener("mouseenter", () => li.classList.add("home_button"));
-  li.addEventListener("mouseleave", () => li.classList.remove("home_button"));
-});
+// document.querySelectorAll(".header_nav li").forEach((li) => {
+//   li.addEventListener("mouseenter", () => li.classList.add("home_button"));
+//   li.addEventListener("mouseleave", () => li.classList.remove("home_button"));
+// });
 
-function btns() {
-  console.log("btn");
-  document.querySelector(".header_nav li").classList.add("home_button");
-  document.querySelector(".header_nav li a").classList.add("translate");
-}
+// function btns() {
+//   console.log("btn");
+//   document.querySelector(".header_nav li").classList.add("home_button");
+//   document.querySelector(".header_nav li a").classList.add("translate");
+// }
 
-function btns_remove() {
-  console.log("remove_btn");
-  document.querySelector(".btn_home").classList.remove("home_button");
-}
+// function btns_remove() {
+//   console.log("remove_btn");
+//   document.querySelector(".btn_home").classList.remove("home_button");
+// }
